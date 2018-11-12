af71f84ae1ad773c3b619941eb88cabde9e93ba6
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-12 04:38:02 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,4 +1,9 @@
 CREATE TABLE bears (
-  id integer PRIMARY KEY,
-  name text,
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
 );
